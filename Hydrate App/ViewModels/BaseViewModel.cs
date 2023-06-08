4fdc7d37f8bc4dfe8d734dda08259fb3754f3b62
Tuukka Viitanen <tuukka.viitanen@insolution.fi>
2023-06-08 19:01:00 +0300

    Added baseviewmodel

---
+++ Hydrate App/ViewModels/BaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Hydrate_App.ViewModels;
+
+public partial class BaseViewModel : ObservableObject
+{
+    [ObservableProperty]
+    string title;
+
+    [ObservableProperty]
+    bool isBusy;
+
+}
