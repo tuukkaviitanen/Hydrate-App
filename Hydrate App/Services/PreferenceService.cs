03db64bc156553f25cfb8ddec4b4b11ef18e4827
Tuukka Viitanen <tuukka.viitanen@insolution.fi>
2023-06-17 13:03:26 +0300

    Added preferenceService for saving settings

---
+++ Hydrate App/Services/PreferenceService.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Hydrate_App.Services;
+public static class PreferenceService
+{
+    public static int HydrateIntervalInMinutes
+    {
+        get => Preferences.Get("HydrateIntervalInMinutes", default(int));
+        set => Preferences.Set("HydrateIntervalInMinutes", value);
+    }
+
+
+    public static bool IsDoNotDisturbEnabled
+    {
+        get => Preferences.Get("IsDoNotDisturbEnabled", default(bool));
+        set => Preferences.Set("IsDoNotDisturbEnabled", value);
+    }
+    public static TimeSpan DoNotDisturbStartTime
+    {
+        get => TimeSpan.FromTicks(Preferences.Get("DoNotDisturbStartTime", TimeSpan.Zero.Ticks));
+        set => Preferences.Set("DoNotDisturbStartTime", value.Ticks);
+    }
+    public static TimeSpan DoNotDisturbEndTime
+    {
+        get => TimeSpan.FromTicks(Preferences.Get("DoNotDisturbEndTime", TimeSpan.Zero.Ticks));
+        set => Preferences.Set("DoNotDisturbEndTime", value.Ticks);
+    }
+}
